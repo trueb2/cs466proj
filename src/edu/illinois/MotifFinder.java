d07f5322434b5d10e6fd0b3dd3984a991d443486
hoho <hwhwang2@illinois.edu>
2016-11-18 18:16:13 -0600

    " Note about what I am going to do in this code "

--- src/edu/illinois/MotifFinder.java
+++ src/edu/illinois/MotifFinder.java
@@ -4,4 +4,23 @@ package edu.illinois;
  * Created by jwtrueb on 11/18/16.
  */
 public class MotifFinder {
+
+    // Load all sequences from *.fa
+    // Load length of Motif from size.txt
+    // randomly choose motif sites for each sequences
+    // recursive doing gibbs sampling
+        // randomly choose one sequence chosSq
+            // recursively go through every subseq in chosSq
+                // calculate Q in
+                    // 1. gen PWM all choosen sites except chosSq
+                    // 2. get Q
+                // calculate P
+                    // 1. Use background PWM all
+                    // 2. get P
+                // Q/P is biggest then record it
+            // replace the choosen motif sites of chosSq by the biggest one
+    //print out all possible sites for each sequence
+    // print out motif weighted array
+
+
 }
