c35c5ee0681362ef76760ce91f2a6d09251146cb
Jacob Trueb <trueb2@illinois.edu>
2016-12-09 12:28:09 -0600

    linter gibbs sampler test

--- src/edu/illinois/tests/GibbsSamplerTest.java
+++ src/edu/illinois/tests/GibbsSamplerTest.java
@@ -10,19 +10,15 @@ import java.util.List;
 import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
-/**
- * Created by jwtrueb on 10/21/16.
- */
 public class GibbsSamplerTest {
 
-    public static final String OUTPUT_DIRECTORY = "out/data/test/";
+    private static final String OUTPUT_DIRECTORY = "out/data/test/";
 
     @Test
     public void readFastaFileTest1() {
-        GibbsSampler gs = new GibbsSampler("sequences.fa","motiflength.txt", OUTPUT_DIRECTORY);
-        assertEquals(10,gs.getSequenceCount());
+        GibbsSampler gs = new GibbsSampler("sequences.fa", "motiflength.txt", OUTPUT_DIRECTORY);
+        assertEquals(10, gs.getSequenceCount());
         List<String> sequences = gs.getSequences();
         assertEquals("TTTAGGCCTATATTCCTCTTAGTTCGGTATCTGGCGCCGGCACACGTCCCAGAATAATCACCCAAGGG" +
                 "CGAGCCCGTTAGAATAATTGTAACCGCCGCATGTCTACAGCAACATCCCACAACGATACCGCGGGAAACAACTCCGCAAACA" +
@@ -43,7 +39,7 @@ public class GibbsSamplerTest {
 
     @Test
     public void findTest1() {
-        GibbsSampler gs = new GibbsSampler("sequences.fa","motiflength.txt", OUTPUT_DIRECTORY);
+        GibbsSampler gs = new GibbsSampler("sequences.fa", "motiflength.txt", OUTPUT_DIRECTORY);
         gs.find(100, 500, new Random(1));
 
         PrintSitesBenchmark.printSitesBenchmark(GibbsSamplerTest.OUTPUT_DIRECTORY);
@@ -52,26 +48,4 @@ public class GibbsSamplerTest {
 
         RelativeEntropyBenchmark.relativeEntropyBenchmark(GibbsSamplerTest.OUTPUT_DIRECTORY);
     }
-
-    //
-//    @Test
-//    public void findTest() {
-//        List<String> seq = generateRandomSequences(5,20);
-//        String motif = "GGGGAGGGG";
-//        Random rand = new Random(System.currentTimeMillis());
-//        List<Integer> correct = new ArrayList<>();
-//        for(int i = 0; i < 5; i++) {
-//            int ran = rand.nextInt( 20 );
-//            correct.add(ran);
-//            seq.set( i, seq.get(i).substring(0,ran).concat(motif).concat( seq.get(i).substring(ran) ) );
-//        }
-//        System.out.println("============= Correct Answer=============");
-//        System.out.println(correct);
-//
-//        GibbsSampler gs = new GibbsSampler();
-//        gs.setSequences(seq);
-//        gs.setMotifLength(motif.length());
-//        System.out.println("============= Result of Gibbs Sampling Algorithm in each iteration =============");
-//        assertTrue(correct.containsAll( gs.gibbsSamplingfinder(15,"./")));
-//    }
 }
