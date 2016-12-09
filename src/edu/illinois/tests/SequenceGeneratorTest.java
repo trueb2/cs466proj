fc332e38dd69a212b04130a96f4af3d0d3236b46
Jacob Trueb <trueb2@illinois.edu>
2016-12-09 12:30:59 -0600

    linted tests

--- src/edu/illinois/tests/SequenceGeneratorTest.java
+++ src/edu/illinois/tests/SequenceGeneratorTest.java
@@ -2,38 +2,18 @@ package edu.illinois.tests;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static edu.illinois.SequenceGenerator.*;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
-/**
- * Created by jwtrueb on 10/21/16.
- */
 public class SequenceGeneratorTest {
 
     @Test
     public void generateRandomSequences1() {
-        List<String> sequences = generateRandomSequences(2,10);
-        assertEquals(2,sequences.size());
-        assertEquals(10,sequences.get(0).length());
-        assertEquals(10,sequences.get(1).length());
+        List<String> sequences = generateRandomSequences(2, 10);
+        assertEquals(2, sequences.size());
+        assertEquals(10, sequences.get(0).length());
+        assertEquals(10, sequences.get(1).length());
     }
-
-//    @Test
-//    public void generateRandomMotif1() {
-//        String motif = generateMotif(1, 10);
-//        assertEquals(10, motif.length());
-//    }
-//
-//    @Test
-//    public void generateBindingSites1() {
-//        ArrayList<String> sequences = generateRandomSequences(10,20);
-//        String motif = generateMotif(1,4);
-//        ArrayList<Integer> bindingSites = generateBindingSites(sequences,motif);
-//        assertTrue(bindingSites.stream().max(Integer::max).get() < 16);
-//        assertTrue(bindingSites.stream().min(Integer::min).get() >= 0);
-//    }
 }
--- src/edu/illinois/tests/SequenceMatrixTest.java
+++ src/edu/illinois/tests/SequenceMatrixTest.java
@@ -9,12 +9,24 @@ import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static org.junit.Assert.assertEquals;
+
 public class SequenceMatrixTest {
 
     @Test
     public void sequenceMatrixTest1() {
-        Random r = new Random();
+        Random r = new Random(2);
         List<String> randomSequences = IntStream.range(0,5).mapToObj(i -> Utils.randomBases(10, r)).collect(Collectors.toList());
         SequenceMatrix sm = new SequenceMatrix(randomSequences);
+        assertEquals("1 1 2 1\n" +
+                "2 2 1 0\n" +
+                "1 0 1 3\n" +
+                "1 0 3 1\n" +
+                "2 1 1 1\n" +
+                "1 2 0 2\n" +
+                "2 1 1 1\n" +
+                "2 1 1 1\n" +
+                "1 1 0 3\n" +
+                "1 0 2 2", sm.toString());
     }
 }
