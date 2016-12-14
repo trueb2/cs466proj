a2eb4232393960aa79226db893e1c775d4b95f2e
Jacob Trueb <trueb2@illinois.edu>
2016-12-14 11:56:48 -0600

    creates histograms and writes stats to file

--- matlab/plot_results.m
+++ matlab/plot_results.m
@@ -19,7 +19,7 @@ function plot_results
     % Read the benchmarks in each folder
     arrayfun(@(f) read_files(outDir,f), folders)
     
-    %% Do some cool graphing
+    %% Do some cool graphing and stats
     icpc = [runs.icpc]';
     ml = [runs.ml]';
     sc = [runs.sc]';
@@ -36,4 +36,31 @@ function plot_results
     ylabel(axes(2), 'Total Relative Entropy');
     ylabel(axes(3), 'Predicted ICPC');
     title('Results of Gibbs Sampling Motif Finder over All Parameters')
+    
+    fid = fopen('stats_output','w');
+    print_stats(fid, 'icpc', icpc);
+    print_stats(fid, 'ml', ml);
+    print_stats(fid, 'sc', sc);
+    print_stats(fid, 'overlaps', o);
+    print_stats(fid, 'entropy', e);
+    print_stats(fid, 'predicted icpc', p);
+    fclose(fid);
 end
+
+function print_stats(fid, name, asdf)
+    fprintf(fid, 'Stats for %s\n', name);
+    fprintf(fid, 'min :: %f\n', min(asdf));
+    fprintf(fid, 'max :: %f\n', max(asdf));
+    fprintf(fid, 'mean :: %f\n', mean(asdf));
+    fprintf(fid, 'mode :: %f\n', mode(asdf));
+    fprintf(fid, 'standard deviation :: %f\n\n', std(double(asdf)));
+    figure();
+    histogram(double(asdf));
+    title(name);
+    saveas(gcf, [name '_histogram.jpg']);
+    close();
+end
+    
+    
+    
+    
